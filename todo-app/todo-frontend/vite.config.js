2bb81f2580be45d4d78d2496a727f2efaf793176
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-11-22 11:50:25 -0600

    Created Dockerfile for todo-frontend. Finished Exercise 12.13

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
+
+// export default defineConfig({
+//   plugins: [react()],
+//   use: {
+//     baseURL: "http://localhost:5173",
+//   },
+//   server: {
+//     proxy: {
+//       "/todos": {
+//         // target: "http://localhost:3000",
+//         // target: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
+//         target: import.meta.env.VITE_BACKEND_URL,
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
